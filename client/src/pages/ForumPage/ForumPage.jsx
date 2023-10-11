0317e86863662b7550ad9303df8b02b60ad78447
Meng Kiat <116622260+mkiats@users.noreply.github.com>
2023-10-11 16:05:09 +0800

    forum page

--- client/src/pages/ForumPage/ForumPage.jsx
+++ client/src/pages/ForumPage/ForumPage.jsx
@@ -1,15 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import "./ForumPage.css"
-import DropdownModuleSearchBox from '../../components/DropdownModuleSearchBox';
+import React, { useState, useEffect } from "react";
+import "./ForumPage.css";
+import DropdownModuleSearchBox from "../../components/DropdownModuleSearchBox";
 
 // For swapping of modules
 function ForumPage() {
     return (
-        <div className="test">
-          <DropdownModuleSearchBox/>
-          <h2 className='temp'>FORUM PAGE - MOD SWAP, BALLOT, POINTS ETC</h2>
+        <div className="page">
+            <DropdownModuleSearchBox />
+            <section>
+                <div className="mod_information">
+                    <div className="course_code">
+                        <h1> Course code </h1>
+                    </div>
+                    <div className="current_index">
+                        <h1> Current Index </h1>
+                    </div>
+                    <div className="desired_index">
+                        <h1> Desired Index </h1>
+                    </div>
+                    <div className="status">
+                        <h1> Status </h1>
+                    </div>
+                </div>
+            </section>
+            <h2 className="temp">FORUM PAGE - MOD SWAP, BALLOT, POINTS ETC</h2>
         </div>
-      );
+    );
 }
 
 export default ForumPage;
