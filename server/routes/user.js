0ec1d2fe7a6bf5578a315568dafdf15ffae54df1
appleworm34 <xavieryeo2@gmail.com>
2023-10-30 18:23:01 +0800

    add credit to user for ballot

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 import express from "express"
 
-import {deleteUser, deleteAllUsers, getUser, registerCourse, addRemoveCourseTaken, getAllUsers, addDesiredIndex, removeDesiredIndex, getUserAddedModules, getUserPlans, updatePlans} from "../controllers/user.js"
+import {deleteUser, deleteAllUsers, getUser, registerCourse, addRemoveCourseTaken, getAllUsers, addDesiredIndex, removeDesiredIndex, getUserAddedModules, getUserPlans, updatePlans, getUserCredit, updateCredit} from "../controllers/user.js"
 
 const router = express.Router();
 
@@ -27,5 +27,8 @@ router.get("/:id/plans", getUserPlans);
 
 router.put("/:id/update_plans", updatePlans);
 
+router.get("/:id/credit", getUserCredit);
+
+router.put("/:id/update_credit", updateCredit);
 
 export default router
